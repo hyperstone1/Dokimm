b577136f24b2026fe198521acae50cc812165b57
hyperstone1 <kingofthegorgon@mail.ru>
2023-10-30 14:59:09 +0300

    fixed: added fully animation for form

--- src/index.js
+++ src/index.js
@@ -7,4 +7,24 @@ import "./js/components/header";
 import { WOW } from "wowjs";
 import "animate.css";
 import "wowjs/css/libs/animate.css";
-new WOW().init();
+new WOW({
+  resetAnimation: false,
+  callback: function (box) {
+    console.log(box);
+    if (box.classList.contains("request__content-text--anim")) {
+      setTimeout(() => {
+        box.classList.add("anim-fill-text");
+      }, 2200);
+    }
+    // the callback is fired every time an animation is started
+    // the argument that is passed in is the DOM node being animated
+  },
+}).init();
+var animatedElement = document.querySelector(
+  ".request__content-wrapper.wow.anim-category"
+);
+animatedElement.addEventListener("wow:in", function () {
+  console.log("appear");
+  animatedElement.classList.add(".wow anim-fill-text");
+  // Код, который выполнится, когда элемент появится на экране
+});
