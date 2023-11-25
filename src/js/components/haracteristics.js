eb44e4fb5a98b8cf79f39b977505878f830401b7
hyperstone1 <kingofthegorgon@mail.ru>
2023-11-25 14:59:20 +0300

    fix: numbers,navigation

--- src/js/components/haracteristics.js
+++ src/js/components/haracteristics.js
@@ -16,6 +16,24 @@ const navCard = document.querySelector(".banner__card-navigation");
 const navCardNext = navCard && navCard.querySelector(".navigation-next");
 const navCardPrev = navCard && navCard.querySelector(".navigation-prev");
 
+const navHaract = document.querySelector(".haracteristics__left-navigation");
+const harPrev = navHaract && navHaract.querySelector(".navigation-prev");
+const harNext = navHaract && navHaract.querySelector(".navigation-next");
+
+const bannerCur = document.querySelector(
+  ".banner__card-pagination .pagination-current"
+);
+const bannerLast = document.querySelector(
+  ".banner__card-pagination .pagination-last"
+);
+
+const curHarac = document.querySelector(
+  ".haracteristics__left-pagination .pagination-current"
+);
+const lastHarac = document.querySelector(
+  ".haracteristics__left-pagination .pagination-last"
+);
+
 const bannerCard1 = new Swiper(".banner__card-slider", {
   slidesPerView: 1,
   spaceBetween: 20,
@@ -23,16 +41,11 @@ const bannerCard1 = new Swiper(".banner__card-slider", {
   slidesPerView: 1,
   on: {
     init: function (swiper) {
-      swiper.slides.forEach((item, id) => {
-        if (id !== swiper.activeIndex) {
-          item.classList.add("no-transition");
-        }
-      });
+      bannerCur.textContent = swiper.activeIndex + 1;
+      bannerLast.textContent = swiper.slides.length;
     },
     slideChange: function (swiper) {
-      swiper.slides.forEach((item) => {
-        item.classList.remove("no-transition");
-      });
+      bannerCur.textContent = swiper.activeIndex + 1;
     },
   },
 });
@@ -81,22 +94,30 @@ navCardPrev.addEventListener("click", () => {
 const haractSlider = new Swiper(".haracteristics__left-slider", {
   slidesPerView: 1,
   spaceBetween: 20,
-  modules: [EffectCreative],
+  modules: [EffectCreative, Navigation],
   speed: 1500,
   slidesPerView: 1,
   effect: "creative",
+  navigation: {
+    prevEl: harPrev,
+    nextEl: harNext,
+  },
+  creativeEffect: {
+    prev: {
+      shadow: true,
+      translate: ["-20%", 0, -1],
+    },
+    next: {
+      translate: ["100%", 0, 0],
+    },
+  },
   on: {
     init: function (swiper) {
-      swiper.slides.forEach((item, id) => {
-        if (id !== swiper.activeIndex) {
-          item.classList.add("no-transition");
-        }
-      });
+      curHarac.textContent = swiper.activeIndex + 1;
+      lastHarac.textContent = swiper.slides.length;
     },
     slideChange: function (swiper) {
-      swiper.slides.forEach((item) => {
-        item.classList.remove("no-transition");
-      });
+      curHarac.textContent = swiper.activeIndex + 1;
     },
   },
 });
