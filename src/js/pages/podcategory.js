deef8045202cfcc39e537772642a94af77bf3441
hyperstone1 <kingofthegorgon@mail.ru>
2023-11-28 02:25:59 +0300

    rebuild

--- src/js/pages/podcategory.js
+++ src/js/pages/podcategory.js
@@ -13,6 +13,7 @@ import remToPx from "../utils/rem";
 import "slick-carousel/slick/slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import $ from "jquery"; // Import jQuery
 
 // const swiper1 = new Swiper(".banner__slider", {
 //   slidesPerView: "auto",
@@ -23,75 +24,78 @@ import "slick-carousel/slick/slick-theme.css";
 const slider = document.querySelector(".banner__slider");
 // $(".").slick({});
 if (slider) {
-  new window.Slick(slider, {
+  $(slider).slick({
     dots: true,
     infinite: true,
     speed: 300,
     slidesToShow: 1,
     centerMode: true,
     variableWidth: true,
+    onAfterChange: function (slider, index) {
+      console.log(slider);
+    },
   });
 }
-const swiper = new Swiper(".banner__slider", {
-  // modules: [EffectCreative],
-  grabCursor: true,
-  centeredSlides: false,
-  slidesPerView: 3,
-  initialSlide: 4,
-  // loop: true,
-  loopAdditionalSlides: 4,
-  spaceBetween: remToPx(8),
-  // effect: "creative",
-  // creativeEffect: {
-  //   prev: {
-  //     shadow: true,
-  //     translate: ["-120%", 0, 0],
-  //   },
-  //   next: {
-  //     shadow: true,
-  //     translate: ["120%", 0, 0],
-  //   },
-  // },
-  // speed: 1000,
-  on: {
-    slideChange: function (swiper) {
-      // setTimeout(() => {
-      //   swiper.update();
-      // }, 1);
-    },
-    slideChangeTransitionStart: function (swiper) {
-      swiper.slides.forEach((item) => {
-        item.style.width = "300px";
-      });
-    },
-    slideChangeTransitionEnd: function (swiper) {
-      swiper.slides.forEach((item) => {
-        item.style.width = "300px";
-      });
-      swiper.slides[swiper.activeIndex].style.width = "300px";
-      swiper.slides[swiper.previousIndex].style.width = "200px";
-      swiper.slides[swiper.activeIndex + 1].style.width = "200px";
-    },
-  },
+// const swiper = new Swiper(".banner__slider", {
+//   // modules: [EffectCreative],
+//   grabCursor: true,
+//   centeredSlides: false,
+//   slidesPerView: 3,
+//   initialSlide: 4,
+//   // loop: true,
+//   loopAdditionalSlides: 4,
+//   spaceBetween: remToPx(8),
+//   // effect: "creative",
+//   // creativeEffect: {
+//   //   prev: {
+//   //     shadow: true,
+//   //     translate: ["-120%", 0, 0],
+//   //   },
+//   //   next: {
+//   //     shadow: true,
+//   //     translate: ["120%", 0, 0],
+//   //   },
+//   // },
+//   // speed: 1000,
+//   on: {
+//     slideChange: function (swiper) {
+//       // setTimeout(() => {
+//       //   swiper.update();
+//       // }, 1);
+//     },
+//     slideChangeTransitionStart: function (swiper) {
+//       swiper.slides.forEach((item) => {
+//         item.style.width = "300px";
+//       });
+//     },
+//     slideChangeTransitionEnd: function (swiper) {
+//       swiper.slides.forEach((item) => {
+//         item.style.width = "300px";
+//       });
+//       swiper.slides[swiper.activeIndex].style.width = "300px";
+//       swiper.slides[swiper.previousIndex].style.width = "200px";
+//       swiper.slides[swiper.activeIndex + 1].style.width = "200px";
+//     },
+//   },
 
-  breakpoints: {
-    769: {
-      slidesPerView: "auto",
-      centeredSlides: true,
-      initialSlide: 4,
-      loop: false,
-      // loopAdditionalSlides: 4,
-      spaceBetween: remToPx(8),
-    },
-  },
-  // effect: "coverflow",
-  // EffectCoverflow: {
-  //   rotate: 0,
-  //   depth: 800,
-  //   slideShadows: true,
-  // },
-  // autoplay: { delay: 5000 },
-});
+//   breakpoints: {
+//     769: {
+//       slidesPerView: "auto",
+//       centeredSlides: true,
+//       initialSlide: 4,
+//       loop: false,
+//       // loopAdditionalSlides: 4,
+//       spaceBetween: remToPx(8),
+//     },
+//   },
+//   // effect: "coverflow",
+//   // EffectCoverflow: {
+//   //   rotate: 0,
+//   //   depth: 800,
+//   //   slideShadows: true,
+//   // },
+//   // autoplay: { delay: 5000 },
+// });
 // window.onresize = queryResizer;
 // queryResizer();
 // function queryResizer() {
